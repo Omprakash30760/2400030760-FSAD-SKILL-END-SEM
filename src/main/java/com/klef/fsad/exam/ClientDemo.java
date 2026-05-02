3f224e87aa114810fec4900930a5d2c075462d27
Om Prakash Jena <omprakashjena1606@gmail.com>
2026-05-02 17:02:49 +0530

    Update Delivery instances with new data

--- src/main/java/com/klef/fsad/exam/ClientDemo.java
+++ src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -14,35 +14,22 @@ public class ClientDemo
         Configuration cfg = new Configuration();
         cfg.configure();
         cfg.addAnnotatedClass(Delivery.class);
-
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
-
         Transaction tx = session.beginTransaction();
-
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-        Delivery d1 = new Delivery(1, "Akhil", sdf.parse("2026-05-02"), "Booked", "Hyderabad", "Vijayawada", 2.5);
-        Delivery d2 = new Delivery(2, "Meera", sdf.parse("2026-05-03"), "In Transit", "Chennai", "Bengaluru", 4.2);
-
+        Delivery d1 = new Delivery(1, "Om Prakash", sdf.parse("2026-05-01"), "Booked", "Hyderabad", "Vijayawada", 5.5);
+        Delivery d2 = new Delivery(2, "Monalisa", sdf.parse("2026-05-07"), "In Transit", "Chennai", "Bengaluru", 6.1);
         session.persist(d1);
         session.persist(d2);
-
         tx.commit();
-
         Transaction tx2 = session.beginTransaction();
-
         Query<?> q = session.createQuery(
         "delete from Delivery where id=?1");
-
         q.setParameter(1, 1);
-
         int n = q.executeUpdate();
-
         System.out.println("Records Deleted = "+n);
-
         tx2.commit();
-
         session.close();
         sf.close();
     }
